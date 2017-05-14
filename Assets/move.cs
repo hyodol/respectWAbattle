877948ca8cbf87248cac8a60e30f87c2376d34db
hyodol <uji.k.chan20@gmail.com>
2017-05-14 21:37:08 +0900

    一旦全部あげる

---
+++ Assets/move.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class move : MonoBehaviour {
+
+	GameObject result = null;
+	GameObject mainCamera;
+	GameObject targetpointA;
+	Camera main;
+	public float speed = 5;
+	public GameObject target;
+
+	void Start (){
+		mainCamera = GameObject.Find ("Main Camera");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		main = mainCamera.GetComponent <Camera> ();
+		Vector3 mousePos = main.ScreenToWorldPoint (Input.mousePosition);
+		Collider2D col = Physics2D.OverlapPoint (mousePos);
+
+		if (Input.GetMouseButtonDown (0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit = new RaycastHit ();
+			if (Physics.Raycast (ray, out hit))
+			{
+
+				result = hit.collider.gameObject;
+					
+				//target = result;
+
+				//targetの座標を取得
+				Vector3 targetposition = result.transform.position;
+
+				//targetの方を向ける
+				transform.LookAt(targetposition);
+
+				while(Vector3.Distance(targetposition, transform.position) > 0.3f)
+				{
+					this.transform.position += new Vector3 (1, 0, 0);
+
+					continue;
+				
+				}
+			}
+		}
+
+
+			
+	}
+}
---
+++ Assets/move_test.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move_test : MonoBehaviour {
+
+	public GameObject target;
+	[SerializeField]
+	private float movement = 1f;
+	[SerializeField]
+	private float rotatespeed = 2f;
+	float moveX = 0f, moveZ = 1f;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find("targetpointA");
+		//Vector3 targetposition = target.transform.position; 
+		rb = this.GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.LookAt (target.transform);
+		movetotarget ();
+
+	}
+
+	void movetotarget () {
+		//while (Vector3.Distance (target.transform.position, this.transform.position) >  0.3f) {
+			rb.velocity = transform.forward * 10f;
+		//}
+	}
+}
