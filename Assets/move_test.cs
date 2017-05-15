4629a89033c2f3d6b60308697bda49f1bf6af6a3
hyodol <uji.k.chan20@gmail.com>
2017-05-16 00:54:15 +0900

    移動処理に条件を付けた

--- Assets/move_test.cs
+++ Assets/move_test.cs
@@ -3,32 +3,29 @@ using System.Collections;
 
 public class move_test : MonoBehaviour {
 
+	private bool arrived;
 	public GameObject target;
 	[SerializeField]
-	private float movement = 1f;
-	[SerializeField]
-	private float rotatespeed = 2f;
-	float moveX = 0f, moveZ = 1f;
+	private float speed = 1f;
 	Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
 		target = GameObject.Find("targetpointA");
-		//Vector3 targetposition = target.transform.position; 
 		rb = this.GetComponent<Rigidbody> ();
-
+		arrived = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		this.transform.LookAt (target.transform);
-		movetotarget ();
+		if (Vector3.Distance (target.transform.position, this.transform.position) > 1f) {
+			MoveToTarget ();	
 
-	}
+		}
+			}
 
-	void movetotarget () {
-		//while (Vector3.Distance (target.transform.position, this.transform.position) >  0.3f) {
-			rb.velocity = transform.forward * 10f;
-		//}
+	void MoveToTarget () {
+		rb.velocity = transform.forward * speed;
 	}
 }
