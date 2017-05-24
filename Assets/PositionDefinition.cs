16fb623747bd56e84d5efb468c782809f7f5b021
hyodol <uji.k.chan20@gmail.com>
2017-05-25 01:41:46 +0900

    メソッドを書く位置を間違えてたので修正。プロトタイプ宣言を削除

--- Assets/PositionDefinition.cs
+++ Assets/PositionDefinition.cs
@@ -7,23 +7,24 @@ public class PositionDefinition : MonoBehaviour {
 	int PosNum = 1;
 	string Direction = "Right";
 	int result;
-	int Target(int, string);
+	//int Target(int string);
 
 	// Use this for initialization
 	void Start () {
 		Positions = GameObject.FindGameObjectsWithTag ("Positions");
 		result = Target (PosNum, Direction);
-		Debug.Log ("result is Position", Target);
+		//Debug.Log ("result is Position", Target);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 	}
-}
 
-int Target (int PosNum, string Direction){
-	if(Direction == Right){
-		return PosNum = PosNum + 1;
+	public int Target (int PosNum, string Direction){
+		if(Direction == "Right"){
+			return PosNum + 1;
+		}
 	}
 }
+
